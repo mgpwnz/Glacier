0718a3167e2290e0596e00310d8a7845c3ce3067
Vitaliy <95574118+mgpwnz@users.noreply.github.com>
2024-11-25 15:35:18 +0200

    upd 0.0.2

--- install.sh
+++ install.sh
@@ -3,6 +3,9 @@
 # Default action
 action="install"
 
+# Define version
+version="v0.0.2"
+
 # Parse options
 option_value() { echo "$1" | sed -e 's%^--[^=]*=%%g; s%^-[^=]*=%%g'; }
 while test $# -gt 0; do
@@ -71,7 +74,7 @@ version: "3.7"
 services:
   glacier-verifier:
     container_name: glacier-verifier
-    image: docker.io/glaciernetwork/glacier-verifier:v0.0.1
+    image: docker.io/glaciernetwork/glacier-verifier:$version
     restart: always
     environment:
       - PRIVATE_KEY=$PRIVATE_KEY
@@ -85,7 +88,9 @@ EOF
 }
 
 update() {
+    # Stop and update container
     docker compose -f "$HOME/glacier/docker-compose.yml" down
+    sed -i "s|image: docker.io/glaciernetwork/glacier-verifier:.*|image: docker.io/glaciernetwork/glacier-verifier:$version|g" "$HOME/glacier/docker-compose.yml"
     docker compose -f "$HOME/glacier/docker-compose.yml" pull
     docker compose -f "$HOME/glacier/docker-compose.yml" up -d
     docker logs -f glacier-verifier
